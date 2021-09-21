5fcb96dc6c3873f5f147b4f0708fc8b96dc32cb4
alifeil <alifiafas@gmail.com>
2021-09-21 14:19:57 +0700

    web

--- app/src/main/java/com/alifeil/notificationpushfirebase/WebActivity.kt
+++ app/src/main/java/com/alifeil/notificationpushfirebase/WebActivity.kt
@@ -2,13 +2,13 @@ package com.alifeil.notificationpushfirebase
 
 import android.annotation.SuppressLint
 import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebViewClient
 import androidx.annotation.RequiresApi
-import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_web.*
 
-class WebActivity : AppCompatActivity(){
+class WebActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web)
